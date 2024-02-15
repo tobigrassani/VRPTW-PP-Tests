55bf74ba8076c774dde815f27503fecfc28d6487
Tobias Grassani <tobigrassani@gmail.com>
2024-02-15 09:58:14 -0300

    Se incluyo lista de marcadores sobre el mapa

--- Ejecutable.py
+++ Ejecutable.py
@@ -34,7 +34,119 @@ def importar_marcadores():
 
 
 # Función para crear el gráfico visualizando los marcadores en un mapa de Folium
+# Función para crear el gráfico visualizando los marcadores en un mapa de Folium
 def crear_grafico():
+    global df
+
+    if df is not None:
+        # Crear un mapa de Folium
+        mapa = folium.Map()
+
+        # Crear una lista para almacenar las coordenadas de los puntos
+        coordenadas = []
+
+        # Crear una lista para almacenar la información de los marcadores
+        lista_marcadores = []
+
+        # Iterar sobre las filas del DataFrame para agregar marcadores al mapa y a la lista de coordenadas
+        for index, row in df.iterrows():
+            coordenadas.append((row['Latitud'], row['Longitud']))
+
+            # Añadir la información del marcador a la lista
+            lista_marcadores.append(f"<b>{row['Cliente']}</b>: {row['Nombre']}<br>")
+
+            # Personalizar el marcador con el nombre del cliente como tooltip y el nombre como contenido del popup
+            if index == 0:  # Si es el primer marcador
+                icono = plugins.BeautifyIcon(
+                    icon="map-marker",
+                    icon_shape="marker",
+                    number=str(index + 1),
+                    border_color="#c20000",
+                    background_color="#c20000",
+                    text_color='white',
+                    inner_icon_style='font-size:1.2em;vertical-align:middle;horizontal-align:middle'
+                )
+            elif index == len(df) - 1:  # Si es el último marcador
+                icono = plugins.BeautifyIcon(
+                    icon="map-marker",
+                    icon_shape="marker",
+                    number=str(index + 1),
+                    border_color="black",
+                    background_color="black",
+                    text_color='white',
+                    inner_icon_style='font-size:1.2em;vertical-align:middle;horizontal-align:middle'
+                )
+            else:
+                icono = plugins.BeautifyIcon(
+                    icon="map-marker",
+                    icon_shape="marker",
+                    number=str(index + 1),
+                    border_color="#2a8c4a",
+                    background_color="#2a8c4a",
+                    text_color='white',
+                    inner_icon_style='font-size:1.3em;vertical-align:middle;horizontal-align:middle'
+                )
+            folium.Marker(location=[row['Latitud'], row['Longitud']],
+                          tooltip="<b><h4>" + str(row['Cliente']) + "</h4></b>",
+                          popup="<b><h5>" + str(row['Cliente']) + "</h5></b>" + "<dd>" + row['Nombre'] + "</dd>",
+                          icon=icono
+                          ).add_to(mapa)
+
+        # Calcular el centro del mapa y el nivel de zoom adecuado
+        latitudes = [coord[0] for coord in coordenadas]
+        longitudes = [coord[1] for coord in coordenadas]
+        centro_latitud = sum(latitudes) / len(latitudes)
+        centro_longitud = sum(longitudes) / len(longitudes)
+        zoom = 10  # Puedes ajustar el nivel de zoom según sea necesario
+
+        # Agregar una polilínea que conecte los puntos en orden
+        folium.PolyLine(coordenadas, color="blue", weight=2.5, opacity=1).add_to(mapa)
+
+        # Establecer el centro y el nivel de zoom del mapa
+        mapa.fit_bounds([(min(latitudes), min(longitudes)), (max(latitudes), max(longitudes))], padding=(3, 3))
+
+        # Añadir la lista de marcadores al mapa
+        lista_html = """
+        <div id="marcadores" style="position: absolute; top: 10px; right: 10px; background-color: white; padding: 10px; border: 1px solid black; z-index: 1000; display: none;">
+          <h3>Marcadores</h3>
+          <ul style="list-style-type:none; padding: 0;">
+        """
+        for i, marcador in enumerate(lista_marcadores):
+            lista_html += f"<li><b>{i + 1}:</b> {marcador}</li>"
+        lista_html += """
+          </ul>
+        </div>
+        """
+        mapa.get_root().html.add_child(folium.Element(lista_html))
+
+        # Añadir un botón para mostrar/ocultar la lista de marcadores
+        boton_html = """
+        <button onclick="toggleMarcadores()">Mostrar/Ocultar Marcadores</button>
+        """
+        mapa.get_root().html.add_child(folium.Element(boton_html))
+
+        # JavaScript para mostrar/ocultar la lista de marcadores
+        javascript = """
+        <script>
+        function toggleMarcadores() {
+          var marcadoresDiv = document.getElementById("marcadores");
+          if (marcadoresDiv.style.display === "none") {
+            marcadoresDiv.style.display = "block";
+          } else {
+            marcadoresDiv.style.display = "none";
+          }
+        }
+        </script>
+        """
+        mapa.get_root().html.add_child(folium.Element(javascript))
+
+        # Guardar el mapa en un archivo HTML
+        mapa.save(mapa_html)
+
+        # Abrir el mapa en el navegador web predeterminado
+        webbrowser.open(mapa_html)
+
+'''def crear_grafico():
     global df
 
     if df is not None:
@@ -84,7 +196,7 @@ def crear_grafico():
                           popup="<b><h5>" + str(row['Cliente']) + "</h5></b>" + "<dd>" +  row['Nombre'] + "</dd>",
                           icon=icono
                           ).add_to(mapa)
-
+            
         # Calcular el centro del mapa y el nivel de zoom adecuado
         latitudes = [coord[0] for coord in coordenadas]
         longitudes = [coord[1] for coord in coordenadas]
@@ -103,7 +215,7 @@ def crear_grafico():
 
         # Abrir el mapa en el navegador web predeterminado
         webbrowser.open(mapa_html)
-
+'''
 # Crear la ventana principal
 ventana = tk.Tk()
 ventana.title("Visualización de Datos")
